98ad5f7ad022404609a7477527b41d3858eda2f4
Efremov Nikita <119881370+DanisArt13@users.noreply.github.com>
2024-05-21 14:15:45 +0300

    Delete trener.py

--- trener.py
+++
@@ -1,9 +0,0 @@
-class Test1:
-    """ Test """
-    def __init__(self, text):
-        self.text = text
-    def __repr__(self):
-        return f'{self.text} Test1'
-class Test2:
-    def __repr__(self):
-        return f'{self.text} Test 2'
